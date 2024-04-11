c0fdca9e5ff6e15d201415a11b0ba5d931d26749
Varun Gole <varungole1999@gmail.com>
2024-04-11 16:30:10 -0400

    initialization

---
+++ wb_initialization.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void zero_wb(float w, float b)
+{
+   w = 0;
+   b = 0;
+}
+
+void one_wb(float w, float b)
+{
+    w = 1;
+    b = 1;
+}
---
+++ wb_initialization.h
@@ -0,0 +1,6 @@
+#ifndef WB_INITIALIZATION
+#define WB_INITIALIZATION
+
+void zero_wb(float w, float b);
+
+#endif
