b979a8abd8ae2547012f3b41d4fafdea2e245778
Varun Gole <varungole1999@gmail.com>
2024-04-11 14:26:17 -0400

    commit

---
+++ activation.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<math.h>
+
+int relu(int x)
+{
+    if(x > 0)
+    {
+        return x;
+    }
+    return 0;
+}
+
+int identity(int x)
+{
+    return x;
+}
+
+int sigmoid(int x)
+{
+    return 1/1+(exp(-x));
+}
+
+int tan_h(int x)
+{
+    int ans = exp(x) + exp(-x);
+    ans /= ans;
+    return ans;
+}
+
+int leaky_relu(int x)
+{
+    int a = 0.01*x;
+    int b = x;
+    if(a>b)
+    {
+        return a;
+    }
+    return b;
+}
+
+double softplus(int x)
+{
+    return log(1 + exp(x));
+}
+
+int main()
+{
+    printf("%d", relu(10));
+    return 0;
+}
